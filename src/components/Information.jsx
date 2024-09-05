59c183b8224968c7db27d55a1dee3fea89093a30
hun2zz <tkdgnsdldkdlel@naver.com>
2024-09-05 23:38:21 +0900

    :white_check_mark: 헤드 부분 완성

---
+++ src/components/Information.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import styles from './Information.module.scss'
+
+const Information = (h1name) => {
+  return (
+    <div>
+      <h1 className={styles.in}></h1>
+    </div>
+  )
+}
+
+export default Information
